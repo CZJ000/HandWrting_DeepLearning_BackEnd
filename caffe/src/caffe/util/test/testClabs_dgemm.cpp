d802f2b277efca84e7566eb5d4742afee8290f6b
CZJ000 <CZJ000>
2019-04-05 13:16:11 +0800

    1111

--- caffe/src/caffe/util/test/testClabs_dgemm.cpp
+++ caffe/src/caffe/util/test/testClabs_dgemm.cpp
@@ -118,14 +118,14 @@ for (i = 0; i < M * N; ++i) {
   _TIMING_STOP_(20)
  
 
- for( i=0;i<M;i++)
-    {
-       for( j=0;j<N;j++)
-       {
-           cout<<c[i*N+j]<<" ";
-       }   
-       cout<<endl;
-    }  
+//  for( i=0;i<M;i++)
+//     {
+//        for( j=0;j<N;j++)
+//        {
+//            cout<<c[i*N+j]<<" ";
+//        }   
+//        cout<<endl;
+//     }  
  
 
    _TIMING_START_
