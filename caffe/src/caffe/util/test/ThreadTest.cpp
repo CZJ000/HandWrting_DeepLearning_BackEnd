9ee2d65d927001f3fc9b9cb12546c936bd819caf
CZJ000 <CZJ000>
2019-04-06 22:58:47 +0800

    thread

---
+++ caffe/src/caffe/util/test/ThreadTest.cpp
@@ -0,0 +1,190 @@
+#include<stdio.h>
+
+#include<stdlib.h>
+
+#include<time.h>
+
+#include<pthread.h>
+
+//定义全局变量，防止越界将数组最大定为200
+
+pthread_t thid[1000][1000];//n*n线程线程id
+
+pthread_t tid[2];//双线程线程id
+
+int pass[1000][1000][2];
+
+int n;//矩阵阶数
+
+int a[1000][1000],b[1000][1000];
+
+int result[1000][1000];
+
+int result1[1000][1000];
+
+//-----------------双线程操作函数-------------------
+
+ void Thread1()
+
+{
+
+   int i,j,m;
+
+   for(i=0;(i<n);i++)
+
+    for(j=0;j<n;j++)
+
+for(m=0;m<n;m++)
+
+{
+
+if(i%2==0)
+
+result[i][j]+=a[i][m]*b[m][j];
+
+}
+
+ }
+
+ void Thread2()
+
+ {
+
+   int i,j,m;
+
+   for(i=0;(i<n);i++)
+
+for(j=0;j<n;j++)
+
+  for(m=0;m<n;m++)
+
+  {
+
+  if(i%2!=0)
+
+  result[i][j]+=a[i][m]*b[m][j];
+
+  }
+
+}
+
+//---------------多线程操作函数----------------------
+
+void Thread(int *p)
+
+{
+
+  int row=p[0];
+
+  int col=p[1];
+
+  int res=0;
+
+  int l;
+
+  for(l=0;l<n;l++)
+
+    res+=a[row][l]*b[l][col];
+
+  result[row][col]=res;
+
+}
+
+//---------------------主函数------------------------
+
+int main()
+
+{
+
+  int t;
+
+  for(t=1;t<200;t++)
+
+  {
+
+  int i,j,m;//循环变量
+
+  double start1,finish1;//时间变量
+
+  double start2,finish2;
+
+  double start3,finish3;
+
+  int r;
+
+  n=t;
+
+  //生成随机数组、输出矩阵a，b
+
+  srand((unsigned) time(NULL));//生成时间种子
+
+  for(i=0;i<n;i++)
+
+   for(j=0;j<n;j++)
+
+   {
+
+      a[i][j]=rand()%11;
+
+  b[i][j]=rand()%11;
+
+   }
+
+//--------------------------n*n多线程运算-------------------
+
+start1=clock();//获得开始时间
+
+for(i=0;i<n;i++)
+
+for(j=0;j<n;j++)
+
+  {
+
+  pass[i][j][0]=i;
+
+  pass[i][j][1]=j;
+
+  r=pthread_create(&thid[i][j],NULL,(void*)Thread,pass[i][j]);
+
+       pthread_join(thid[i][j],NULL);//!!!!!!!!!!!!
+
+  }
+
+finish1=clock();//获得结束时间
+
+//----------------------双线程运算---------------------------
+
+start3=clock();
+
+pthread_create(&tid[0],NULL,(void*)Thread1,NULL);
+
+pthread_join(tid[0],NULL);//!!!!!!!!!!!!
+
+pthread_create(&tid[1],NULL,(void*)Thread2,NULL);
+
+pthread_join(tid[1],NULL);//!!!!!!!!!!!!
+
+finish3=clock();
+
+//---------------------单线程运算-----------------------------
+
+start2=clock();
+
+for(i=0;i<n;i++)
+
+for(j=0;j<n;j++)
+
+  for(m=0;m<n;m++)
+
+  result[i][j]+=a[i][m]*b[m][j];
+
+finish2=clock();
+
+printf("矩阵阶数: %3d |  Single: %10g  | Doube: %10g | n*n: %10g\n",t,(finish2-start2),(finish3-start3),(finish1-start1));
+
+ }
+
+ return 0;
+
+}
+
--- caffe/src/caffe/util/test/testClabs_dgemm.cpp
+++ caffe/src/caffe/util/test/testClabs_dgemm.cpp
@@ -15,6 +15,22 @@ using namespace std;
 
 #define TEST_NTIMES 100;
 
+struct mypara
+{
+       const int M;//参数1
+       const int N;//参数2
+       const int K;
+       const float alpha;
+       const float *A;
+       const float *B;
+       const float beta;
+       float *C;
+       int i;
+       int e;
+       int k;
+}
+pthread_t t[100];  
+
 void matrix_mul_vector_neon(const int M, 
      const int N,
      const int K,   
@@ -153,6 +169,14 @@ for (i = 0; i < M * N; ++i) {
 //     }  
  
 
+  _TIMING_START_
+   for (i = 0; i < 1; ++i) {
+     matrix_mul_vector_neon_thread( M, N, K, 1.0f, matrix_A_data,matrix_B_data,0.0f,c1);
+   }
+   _TIMING_STOP_(1)
+
+
+
    _TIMING_START_
    for (i = 0; i < 1; ++i) {
     cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, M, N, K, 1, matrix_A_data, K, matrix_B_data, N, 0, matrix_C_data, N);
@@ -199,6 +223,246 @@ void matrix_normal(const int M,
 
 
 }
+
+
+
+
+matrix_mul_vector_neon_4by4_thread(void *arg)
+{
+
+       mypara *pstru;
+       pstru = (* struct mypara) arg;
+      //  pstru->;//参数1
+      //  pstru->para2;//参数2 
+      int M,N,K;
+      M=pstru->M;
+      N=pstru->N;
+      K=pstru->K;
+      float alpha=pstru->alpha;
+       float beta=pstru->beta;
+      float* A=pstru->A;
+      float* B=pstru->B;
+      float* C=pstru->C;
+
+
+    int i, j,e;
+
+    i=pstru->i;
+    j=pstru->j;
+    e=pstru->e;
+
+    float32x4_t vc0 = vdupq_n_f32(0.0f);
+    float32x4_t vc1 = vdupq_n_f32(0.0f);
+    float32x4_t vc2 = vdupq_n_f32(0.0f);
+    float32x4_t vc3 = vdupq_n_f32(0.0f);
+    for ( j= 0; j < K; j++) 
+    {
+                  //B K*N bb 
+        float32x4_t vb =vld1q_f32(B+j*N+e); // vget(&B[k][4]);   
+        //vfmaq_f32 混合   c=a*b+c  
+        vc0=vmlaq_f32( vc0,vdupq_n_f32(A[i*K+j]), vb);  
+        //  cout<<"vc0 temp:"<<endl;
+        //  float temp[4];
+      // vst1q_f32(temp, vc0);
+        // for(i=0;i<4;i++)
+        // {
+        //   cout<<temp[i]<<" ";
+        // }
+        // cout<<endl;
+        vc1=vmlaq_f32(vc1,vdupq_n_f32(A[(i+1)*K+j]), vb);
+        vc2=vmlaq_f32(vc2,vdupq_n_f32(A[(i+2)*K+j]), vb);
+        vc3=vmlaq_f32(vc3,vdupq_n_f32(A[(i+3)*K+j]), vb);
+    }  
+        // C M*N
+        float32x4_t c0 =vld1q_f32(C+i*N+e);
+        float32x4_t c1 = vld1q_f32(C+(i+1)*N+e);
+        float32x4_t c2 = vld1q_f32(C+(i+2)*N+e);
+        float32x4_t c3 =vld1q_f32(C+(i+3)*N+e);
+
+
+        float32x4_t c0_b =vmulq_f32(c0, vbeta);
+        
+        float32x4_t c1_b = vmulq_f32(c1, vbeta);
+      
+        float32x4_t c2_b = vmulq_f32(c2, vbeta);
+      
+        float32x4_t c3_b =vmulq_f32(c3, vbeta);
+    
+
+
+      float32x4_t c0_r= vaddq_f32(vmulq_f32(vc0, valpha),c0_b);
+      float32x4_t c1_r= vaddq_f32(vmulq_f32(vc0, valpha),c1_b);
+      float32x4_t c2_r= vaddq_f32(vmulq_f32(vc0, valpha),c2_b);
+      float32x4_t c3_r= vaddq_f32(vmulq_f32(vc0, valpha),c3_b);
+
+
+        vst1q_f32(C+i*N+e,c0_r);
+      // cout<<"c1 temp:"<<endl;
+      //  float temp[4];
+      //vst1q_f32(temp, vaddq_f32(vmulq_f32(vc1, valpha), vmulq_f32(c1, vbeta)));
+        vst1q_f32(C+(i+1)*N+e,c1_r);
+      // for(i=0;i<4;i++)
+      //   {
+      //     cout<<C[i]<<" ";
+      //   }
+      //   cout<<endl;
+        vst1q_f32(C+(i+2)*N+e,c2_r);
+        vst1q_f32(C+(i+3)*N+e,c3_r);
+}
+
+
+matrix_mul_vector_neon_colnot4_thread(void *arg)
+{
+
+       mypara *pstru;
+       pstru = (* struct mypara) arg;
+      //  pstru->;//参数1
+      //  pstru->para2;//参数2 
+      int M,N,K;
+      M=pstru->M;
+      N=pstru->N;
+      K=pstru->K;
+      float alpha=pstru->alpha;
+       float beta=pstru->beta;
+      float* A=pstru->A;
+      float* B=pstru->B;
+      float* C=pstru->C;
+
+
+    int i, j,e;
+
+    i=pstru->i;
+    j=pstru->j;
+    e=pstru->e;
+
+    int p=0,l,q;
+        for(l=0;l<4;l++)
+        {
+          for(q=e;q<N;q++)
+          {
+            float sum=0;
+            for(p=0;p<K;p++)
+            {
+                sum+=A[(i+l)*K+p]*B[p*N+q];
+            }
+            C[(i+l)*N+q]=sum;
+          }     
+        } 
+
+ 
+}
+
+
+
+matrix_mul_vector_neon_rownot4_thread(void *arg)
+{
+
+       mypara *pstru;
+       pstru = (* struct mypara) arg;
+      //  pstru->;//参数1
+      //  pstru->para2;//参数2 
+      int M,N,K;
+      M=pstru->M;
+      N=pstru->N;
+      K=pstru->K;
+      float alpha=pstru->alpha;
+       float beta=pstru->beta;
+      float* A=pstru->A;
+      float* B=pstru->B;
+      float* C=pstru->C;
+
+
+    int i, j,e;
+
+    i=pstru->i;
+    j=pstru->j;
+    e=pstru->e;
+
+  int p=0;
+  for(;i<M;i++)
+  {
+    for(e=0;e<N;e++)
+    {
+      float sum=0;
+        for(p=0;p<K;p++)
+        {
+          sum+=A[i*K+p]*B[p*N+e];
+
+        }
+        C[i*N+e]=sum;
+    }
+  }
+
+ 
+}
+
+
+
+
+matrix_mul_vector_neon_thread( const int M, 
+     const int N,
+     const int K,   
+     const float alpha, 
+     const float *A, 
+     const float *B, 
+     const float beta,    
+      float *C)
+{
+  int a=0;
+for ( i = 0; i <=M-4; i+=4) 
+  {
+       for(e=0;e<=N-4;e+=4)
+      {
+          struct mypara pstru;
+          pstru->M=M;
+          pstru->N=N;
+          pstru->K=K;
+          pstru->alpha=alpha;
+          pstru->A=A;
+          pstru->B=B;
+          pstru->C=C;
+          pstru->i=i;
+          pstru->e=e;
+          pthread_create(&t[a], NULL, matrix_mul_vector_neon_4by4_thread,&(pstru));
+          pthread_join(&t[a],NULL);
+          a++;
+      }
+      if(e<N)
+    {
+         struct mypara pstru;
+          pstru->M=M;
+          pstru->N=N;
+          pstru->K=K;
+          pstru->alpha=alpha;
+          pstru->A=A;
+          pstru->B=B;
+          pstru->C=C;
+          pstru->i=i;
+          pstru->e=e;
+          pthread_create(&t[a], NULL, matrix_mul_vector_neon_colnot4_thread,&(pstru));
+          pthread_join(&t[a],NULL);
+          a++;
+    }
+}
+
+          struct mypara pstru;
+          pstru->M=M;
+          pstru->N=N;
+          pstru->K=K;
+          pstru->alpha=alpha;
+          pstru->A=A;
+          pstru->B=B;
+          pstru->C=C;
+          pstru->i=i;
+          pstru->e=e;
+          pthread_create(&t[a], NULL, matrix_mul_vector_neon_rownot4_thread,&(pstru));
+          pthread_join(&t[a],NULL);
+          a++;
+}
+
+
+
+
 
 
 void matrix_mul_vector_neon( 
