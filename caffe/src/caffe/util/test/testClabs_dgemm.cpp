be32dca43f6a6d402f6196f96996c8c65b542aea
CZJ000 <CZJ000>
2019-04-07 11:20:37 +0800

    ++++

--- caffe/src/caffe/util/test/testClabs_dgemm.cpp
+++ caffe/src/caffe/util/test/testClabs_dgemm.cpp
@@ -569,7 +569,7 @@ for ( i = 0; i <=M-4; i+=4)
                   //B K*N bb 
         float32x4_t vb =vld1q_f32(B+j*N+e); // vget(&B[k][4]);   
 
-       float32x4_t va =vld1q_f32(A[j*K+i])
+       float32x4_t va =vld1q_f32(A+j*K+i)
         //vfmaq_f32 混合   c=a*b+c     A[i*K+j]
 
         vc0=vmlaq_f32( vc0,va, vb);  
