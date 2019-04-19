4123dd31f46db1abc55014265ba1d7b552c86aac
CZJ000 <CZJ000>
2019-04-19 13:23:34 +0800

    ++

--- Django/Django/view.py
+++ Django/Django/view.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
-from django.views.decorators import csrf
 import os
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.csrf import csrf_exempt
 import base64
 from . import GetRectangle
 import json
 
+# 接收POST请求数据
+def search_post(request):
+    ctx = {}
+    if request.POST:
+        ctx['rlt'] = request.POST['q']
+    return render(request, "post.html", ctx)
 
 
+def select_pic(request):
+    return render(request, "select.html")
+
+@csrf_exempt
 def upload_ajax(request):
         if request.method == 'POST':
             img = request.POST.get('image')
@@ -20,7 +28,7 @@ def upload_ajax(request):
             img = img[len(header):]
             imagedata = base64.b64decode(img)
             #print(imagedata)
-            path='./1.png'
+            path='E:/1.png'
             file = open(path, "wb")
             file.write(imagedata)
             file.close()
@@ -33,15 +41,3 @@ def upload_ajax(request):
             status = 0
             result = "Error!"
             return HttpResponse(str)
-
-
-def search_post(request):
-    ctx = {}
-    if request.POST:
-        ctx['rlt'] = request.POST['q']
-    return render(request, "post.html", ctx)
-
-
-def select_pic(request):
-    return render(request, "select.html")
-
