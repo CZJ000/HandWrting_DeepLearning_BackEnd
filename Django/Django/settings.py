5ff3f6836de2658010c9992c1e63c2ee36ea1254
CZJ000 <CZJ000>
2019-04-19 12:54:27 +0800

    ++

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -26,27 +26,6 @@ SECRET_KEY = 'ocep$p_uj@j$b507=2&cqk+m=4k8=9@*a$#-1cg21_)*f(0o&1'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_METHODS = (
-    'DELETE',
-    'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT',
-)
-CORS_ALLOW_HEADERS = (
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-)
-
 
 # Application definition
 
